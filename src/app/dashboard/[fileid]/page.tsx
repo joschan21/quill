4573de15e881069bf826f30a73a30f301ebba038
Joscha <neske.joscha@gmail.com>
2023-09-26 17:06:26 +0200

    improve comments for deployment & support plan info

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,6 +1,7 @@
 import ChatWrapper from '@/components/chat/ChatWrapper'
 import PdfRenderer from '@/components/PdfRenderer'
 import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { notFound, redirect } from 'next/navigation'
 
@@ -11,7 +12,6 @@ interface PageProps {
 }
 
 const Page = async ({ params }: PageProps) => {
-  // retrieve the file id
   const { fileid } = params
 
   const { getUser } = getKindeServerSession()
@@ -20,7 +20,6 @@ const Page = async ({ params }: PageProps) => {
   if (!user || !user.id)
     redirect(`/auth-callback?origin=dashboard/${fileid}`)
 
-  // make database call
   const file = await db.file.findFirst({
     where: {
       id: fileid,
@@ -30,18 +29,21 @@ const Page = async ({ params }: PageProps) => {
 
   if (!file) notFound()
 
+  const plan = await getUserSubscriptionPlan()
+
   return (
     <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
       <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
-        {/* left side */}
+        {/* Left sidebar & main wrapper */}
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+            {/* Main area */}
             <PdfRenderer url={file.url} />
           </div>
         </div>
 
         <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
-          <ChatWrapper fileId={file.id} />
+          <ChatWrapper isSubscribed={plan.isSubscribed} fileId={file.id} />
         </div>
       </div>
     </div>
