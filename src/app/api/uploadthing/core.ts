36fe241c39b707ad5ca0486674acbd645db04390
Gaurang Khera <88822600+gaurangkhera5188@users.noreply.github.com>
2023-10-13 19:24:06 +0530

    Allow users to upload files without failing due to namespace error in Pinecone

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,141 +1,77 @@
-import { db } from '@/db'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import {
-  createUploadthing,
-  type FileRouter,
-} from 'uploadthing/next'
+import { db } from '@/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { createUploadthing } from 'uploadthing/next';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { getPineconeClient } from '@/lib/pinecone';
 
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { getPineconeClient } from '@/lib/pinecone'
-import { getUserSubscriptionPlan } from '@/lib/stripe'
-import { PLANS } from '@/config/stripe'
-
-const f = createUploadthing()
+const f = createUploadthing();
 
 const middleware = async () => {
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
-
-  if (!user || !user.id) throw new Error('Unauthorized')
-
-  const subscriptionPlan = await getUserSubscriptionPlan()
-
-  return { subscriptionPlan, userId: user.id }
-}
-
-const onUploadComplete = async ({
-  metadata,
-  file,
-}: {
-  metadata: Awaited<ReturnType<typeof middleware>>
-  file: {
-    key: string
-    name: string
-    url: string
-  }
-}) => {
-  const isFileExist = await db.file.findFirst({
-    where: {
-      key: file.key,
-    },
-  })
-
-  if (isFileExist) return
-
-  const createdFile = await db.file.create({
-    data: {
-      key: file.key,
-      name: file.name,
-      userId: metadata.userId,
-      url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
-      uploadStatus: 'PROCESSING',
-    },
-  })
-
-  try {
-    const response = await fetch(
-      `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
-    )
-
-    const blob = await response.blob()
-
-    const loader = new PDFLoader(blob)
-
-    const pageLevelDocs = await loader.load()
-
-    const pagesAmt = pageLevelDocs.length
-
-    const { subscriptionPlan } = metadata
-    const { isSubscribed } = subscriptionPlan
-
-    const isProExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Pro')!.pagesPerPdf
-    const isFreeExceeded =
-      pagesAmt >
-      PLANS.find((plan) => plan.name === 'Free')!
-        .pagesPerPdf
-
-    if (
-      (isSubscribed && isProExceeded) ||
-      (!isSubscribed && isFreeExceeded)
-    ) {
-      await db.file.update({
-        data: {
-          uploadStatus: 'FAILED',
-        },
-        where: {
-          id: createdFile.id,
-        },
-      })
-    }
-
-    // vectorize and index entire document
-    const pinecone = await getPineconeClient()
-    const pineconeIndex = pinecone.Index('quill')
-
-    const embeddings = new OpenAIEmbeddings({
-      openAIApiKey: process.env.OPENAI_API_KEY,
-    })
-
-    await PineconeStore.fromDocuments(
-      pageLevelDocs,
-      embeddings,
-      {
-        pineconeIndex,
-        namespace: createdFile.id,
-      }
-    )
-
-    await db.file.update({
-      data: {
-        uploadStatus: 'SUCCESS',
-      },
-      where: {
-        id: createdFile.id,
-      },
-    })
-  } catch (err) {
-    await db.file.update({
-      data: {
-        uploadStatus: 'FAILED',
-      },
-      where: {
-        id: createdFile.id,
-      },
-    })
-  }
-}
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+  if (!user || !user.id) throw new Error('Unauthorized');
+  return { userId: user.id };
+};
 
 export const ourFileRouter = {
-  freePlanUploader: f({ pdf: { maxFileSize: '4MB' } })
-    .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
-  proPlanUploader: f({ pdf: { maxFileSize: '16MB' } })
-    .middleware(middleware)
-    .onUploadComplete(onUploadComplete),
-} satisfies FileRouter
+  pdfUploader: f({ pdf: { maxFileSize: '4MB' } }).middleware(async ({ req }) => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+    if (!user || !user.id) throw new Error('Unauthorized');
+    return { userId: user.id };
+  }).onUploadComplete(async ({ metadata, file }: { metadata: any; file: any }) => {
+    const isFileExist = await db.file.findFirst({ where: { key: file.key } });
+    if (isFileExist) return;
 
-export type OurFileRouter = typeof ourFileRouter
+    const createdFile = await db.file.create({
+      data: {
+        key: file.key,
+        name: file.name,
+        userId: metadata.userId,
+        url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+        uploadStatus: 'PROCESSING',
+      },
+    });
+
+    try {
+      const response = await fetch(`https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`);
+      const blob = await response.blob();
+      const loader = new PDFLoader(blob);
+      const pageLevelDocs = await loader.load();
+      const pagesAmt = pageLevelDocs.length;
+
+      // Create a single Pinecone index for all data
+      const pinecone = await getPineconeClient();
+      const pineconeIndex = pinecone.Index('quill'); // Use a single index name
+
+      // Add a 'dataset' field to the data to distinguish the source
+      const combinedData = pageLevelDocs.map((document) => {
+        return {
+          ...document,
+          dataset: 'pdf', // Use a field to indicate the source dataset (e.g., 'pdf')
+        };
+      });
+
+      const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY });
+
+      await PineconeStore.fromDocuments(combinedData, embeddings, {
+        //@ts-ignore
+        pineconeIndex,
+      });
+
+      await db.file.update({
+        data: { uploadStatus: 'SUCCESS' },
+        where: { id: createdFile.id },
+      });
+    } catch (err) {
+      await db.file.update({
+        data: { uploadStatus: 'FAILED' },
+        where: { id: createdFile.id },
+      });
+    }
+  }),
+};
+
+export type OurFileRouter = typeof ourFileRouter;
