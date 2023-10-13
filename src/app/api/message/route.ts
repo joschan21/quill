39026b887594152ea148ddc508ee4b14276e0a03
Gaurang Khera <88822600+gaurangkhera5188@users.noreply.github.com>
2023-10-13 19:23:18 +0530

    Fix route.ts to provide vectorized content without namespaces

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,39 +1,29 @@
-import { db } from '@/db'
-import { openai } from '@/lib/openai'
-import { getPineconeClient } from '@/lib/pinecone'
-import { SendMessageValidator } from '@/lib/validators/SendMessageValidator'
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
-import { PineconeStore } from 'langchain/vectorstores/pinecone'
-import { NextRequest } from 'next/server'
-
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { db } from '@/db';
+import { openai } from '@/lib/openai';
+import { getPineconeClient } from '@/lib/pinecone';
+import { sendMessageValidator } from '@/lib/sendMessageValidator';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+import { NextRequest } from 'next/server';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
 
 export const POST = async (req: NextRequest) => {
-  // endpoint for asking a question to a pdf file
+  const body = await req.json();
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+  const { id: userId } = user;
 
-  const body = await req.json()
+  if (!userId) {
+    return new Response('Unauthorized', { status: 401 });
+  }
 
-  const { getUser } = getKindeServerSession()
-  const user = getUser()
+  const { fileId, message } = sendMessageValidator.parse(body);
+  const file = await db.file.findFirst({ where: { id: fileId, userId } });
 
-  const { id: userId } = user
-
-  if (!userId)
-    return new Response('Unauthorized', { status: 401 })
-
-  const { fileId, message } =
-    SendMessageValidator.parse(body)
-
-  const file = await db.file.findFirst({
-    where: {
-      id: fileId,
-      userId,
-    },
-  })
-
-  if (!file)
-    return new Response('Not found', { status: 404 })
+  if (!file) {
+    return new Response('Not found', { status: 404 });
+  }
 
   await db.message.create({
     data: {
@@ -44,89 +34,71 @@ export const POST = async (req: NextRequest) => {
     },
   })
 
-  // 1: vectorize message
-  const embeddings = new OpenAIEmbeddings({
-    openAIApiKey: process.env.OPENAI_API_KEY,
-  })
+  // 1: Vectorize message
+  const embeddings = new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY });
 
-  const pinecone = await getPineconeClient()
-  const pineconeIndex = pinecone.Index('quill')
+  // Initialize the Pinecone vector store
+  const pinecone = await getPineconeClient();
+  const pineconeIndex = pinecone.Index('quill'); // Use a single index name
 
-  const vectorStore = await PineconeStore.fromExistingIndex(
-    embeddings,
-    {
-      pineconeIndex,
-      namespace: file.id,
-    }
-  )
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, { pineconeIndex }); 
 
-  const results = await vectorStore.similaritySearch(
-    message,
-    4
-  )
+  // console.log(vectorStore)
 
-  const prevMessages = await db.message.findMany({
-    where: {
-      fileId,
-    },
-    orderBy: {
-      createdAt: 'asc',
-    },
-    take: 6,
-  })
+  try {
+    // Search for similar messages using the file ID as context
+    const results = await vectorStore.similaritySearch(message, 4);
+    const prevMessages = await db.message.findMany({
+      where: { fileId },
+      orderBy: { createdAt: 'asc' },
+      take: 6,
+    });
+    const formattedPrevMessages = prevMessages.map((msg) => ({
+      role: msg.isUserMessage ? 'user' : 'assistant',
+      content: msg.text,
+    }));
+    
+    // Construct a context string with previous conversation, results, and user input
+    const context = `PREVIOUS CONVERSATION:${formattedPrevMessages.map((msg) => {
+      if (msg.role === 'user') return `User:${msg.content}\n`;
+      return `Assistant:${msg.content}\n`;
+    })}CONTEXT:${results.map((r) => r.pageContent).join('\n\n')}USER INPUT:${message}`;
 
-  const formattedPrevMessages = prevMessages.map((msg) => ({
-    role: msg.isUserMessage
-      ? ('user' as const)
-      : ('assistant' as const),
-    content: msg.text,
-  }))
+    console.log(context)
 
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    temperature: 0,
-    stream: true,
-    messages: [
-      {
-        role: 'system',
-        content:
-          'Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.',
-      },
-      {
-        role: 'user',
-        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
-        
-  \n----------------\n
-  
-  PREVIOUS CONVERSATION:
-  ${formattedPrevMessages.map((message) => {
-    if (message.role === 'user')
-      return `User: ${message.content}\n`
-    return `Assistant: ${message.content}\n`
-  })}
-  
-  \n----------------\n
-  
-  CONTEXT:
-  ${results.map((r) => r.pageContent).join('\n\n')}
-  
-  USER INPUT: ${message}`,
-      },
-    ],
-  })
-
-  const stream = OpenAIStream(response, {
-    async onCompletion(completion) {
-      await db.message.create({
-        data: {
-          text: completion,
-          isUserMessage: false,
-          fileId,
-          userId,
+    // Use a system message to instruct the model
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      temperature: 0.7, // Adjust the temperature as needed
+      stream: true,
+      messages: [
+        {
+          role: 'system',
+          content: 'You have access to a PDF document. Please use the information from the document to answer the user\'s question.',
         },
-      })
-    },
-  })
+        {
+          role: 'user',
+          content: context, // Provide the context here
+        },
+      ],
+    });
 
-  return new StreamingTextResponse(stream)
-}
+    const stream = OpenAIStream(response, {
+      async onCompletion(completion) {
+        await db.message.create({
+          data: {
+            text: completion,
+            isUserMessage: false,
+            fileId,
+            userId,
+          },
+        });
+      },
+    });
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error('Error searching for similar messages:', error);
+    return new Response('InternalServerError', { status: 500 });
+  }
+};
