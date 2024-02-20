457a22cc9917b62f35895c489f60265a094832e0
mcpdigital <internet@mcpdigital.com>
2024-02-19 23:40:50 -0300

    removed paypal fixed aws pdf file link

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -73,7 +73,7 @@ export const appRouter = router({
     const stripeSession = await stripe.checkout.sessions.create({
       success_url: billingUrl,
       cancel_url: billingUrl,
-      payment_method_types: ["card", "paypal"],
+      payment_method_types: ["card"],
       mode: "subscription",
       billing_address_collection: "auto",
       line_items: [
